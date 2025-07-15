d10f15be25a1b6ed241b9a72733b811c7c3c8c95
gimohc <mrlenovog50@gmail.com>
2025-07-15 13:31:04 +0300

    Initial implementation of services

--- src/main/java/com/training/thymeleafmid/student/StudentService.java
+++ src/main/java/com/training/thymeleafmid/student/StudentService.java
@@ -1,4 +1,26 @@
 package com.training.thymeleafmid.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+    private final PasswordEncoder encoder;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository, PasswordEncoder passwordEncoder) {
+        this.studentRepository = studentRepository;
+        this.encoder = passwordEncoder;
+    }
+
+    public void saveStudent(Student student){
+        student.setPassword(encoder.encode(student.getPassword()));
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Student student){
+        studentRepository.delete(student);
+    }
 }
--- src/main/java/com/training/thymeleafmid/teacher/TeacherService.java
+++ src/main/java/com/training/thymeleafmid/teacher/TeacherService.java
@@ -1,4 +1,22 @@
 package com.training.thymeleafmid.teacher;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TeacherService {
+    private final TeacherRepository teacherRepository;
+    private final PasswordEncoder encoder;
+
+    @Autowired
+    public TeacherService(TeacherRepository teacherRepository, PasswordEncoder passwordEncoder) {
+        this.teacherRepository = teacherRepository;
+        this.encoder = passwordEncoder;
+    }
+
+    public void saveTeacher(Teacher teacher){
+        teacher.setPassword(encoder.encode(teacher.getPassword()));
+        teacherRepository.save(teacher);
+    }
 }
