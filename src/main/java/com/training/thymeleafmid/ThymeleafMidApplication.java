2e737bd47e121738ed19cedb5c4085c55a348740
gimohc <mrlenovog50@gmail.com>
2025-07-15 12:27:40 +0300

    Initial commit

---
+++ src/main/java/com/training/thymeleafmid/ThymeleafMidApplication.java
@@ -0,0 +1,13 @@
+package com.training.thymeleafmid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafMidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafMidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/thymeleafmid/ThymeleafMidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.thymeleafmid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafMidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
