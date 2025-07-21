e103a5b3051f439dfa00868db01b525b3936d923
gimohc <mrlenovog50@gmail.com>
2025-07-21 10:04:55 +0300

    Implemented controller methods in service

--- src/main/java/com/training/thymeleafmid/student/StudentService.java
+++ src/main/java/com/training/thymeleafmid/student/StudentService.java
@@ -1,8 +1,13 @@
 package com.training.thymeleafmid.student;
 
+import com.training.thymeleafmid.Exceptions.StudentNotFoundException;
+import com.training.thymeleafmid.LoginRequest;
+import com.training.thymeleafmid.teacher.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -14,13 +19,35 @@ public class StudentService {
         this.studentRepository = studentRepository;
         this.encoder = passwordEncoder;
     }
+    public boolean validateLogin(LoginRequest request) {
+        String password = studentRepository.findPasswordById(request.getId());
+        return encoder.matches(request.getPassword(), password);
 
-    public void saveStudent(Student student){
-        student.setPassword(encoder.encode(student.getPassword()));
-        studentRepository.save(student);
     }
+    public Student findById(long id){
+        Optional<Student> studentOpt = studentRepository.findById(id);
+        if(studentOpt.isPresent())
+            return studentOpt.get();
+        throw new StudentNotFoundException();
+    }
+    public void saveStudent(Student student) {
 
+        if (student.getPassword() == null || student.getPassword().isEmpty()) {
+            student.setPassword(student.getPassword());
+        } else {
+            student.setPassword(encoder.encode(student.getPassword()));
+        }
+        studentRepository.save(student);
+
+    }
+    public Teacher getTeacherByStudentId(long studentId) {
+        return findById(studentId).getTeacher();
+    }
     public void deleteStudent(Student student){
         studentRepository.delete(student);
+    }
+    public void saveNewStudent(Student student){
+        student.setPassword(encoder.encode(student.getPassword()));
+        studentRepository.save(student);
     }
 }
--- src/main/java/com/training/thymeleafmid/teacher/TeacherService.java
+++ src/main/java/com/training/thymeleafmid/teacher/TeacherService.java
@@ -1,22 +1,77 @@
 package com.training.thymeleafmid.teacher;
 
+import com.training.thymeleafmid.Exceptions.TeacherNotFoundException;
+import com.training.thymeleafmid.LoginRequest;
+import com.training.thymeleafmid.student.Student;
+import com.training.thymeleafmid.student.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class TeacherService {
     private final TeacherRepository teacherRepository;
     private final PasswordEncoder encoder;
+    private final StudentService studentService;
 
     @Autowired
-    public TeacherService(TeacherRepository teacherRepository, PasswordEncoder passwordEncoder) {
+    public TeacherService(TeacherRepository teacherRepository, StudentService studentService
+                          //, StudentRepository studentRepository
+                          , PasswordEncoder passwordEncoder) {
         this.teacherRepository = teacherRepository;
         this.encoder = passwordEncoder;
+        //this.studentRepository = studentRepository;
+        this.studentService = studentService;
     }
-
-    public void saveTeacher(Teacher teacher){
+    public Teacher findById(long id){
+        Optional<Teacher> teacherOpt = teacherRepository.findById(id);
+        if(teacherOpt.isPresent())
+            return teacherOpt.get();
+        throw new TeacherNotFoundException();
+    }
+    @Transactional
+    public void addStudent(long studentId, long teacherId) {
+        Student student = studentService.findById(studentId);
+        Teacher teacher = findById(teacherId);
+        teacher.addStudent(student);
+        student.setTeacher(teacher);
+        teacherRepository.save(teacher);
+        studentService.saveStudent(student);
+    }
+    @Transactional
+    public void removeStudent(long studentId, long teacherId) {
+        Student student = studentService.findById(studentId);
+        Teacher teacher = findById(teacherId);
+        teacher.removeStudent(student);
+        student.setTeacher(null);
+        teacherRepository.save(teacher);
+        studentService.saveStudent(student);
+    }
+    public Iterable<Teacher> findAll() {
+        return teacherRepository.findAll();
+    }
+    public boolean validateLogin(LoginRequest request) {
+        String password = teacherRepository.findPasswordById(request.getId());
+        return encoder.matches(request.getPassword(), password);
+    }
+    public void saveNewTeacher(Teacher teacher) {
         teacher.setPassword(encoder.encode(teacher.getPassword()));
         teacherRepository.save(teacher);
+    }
+    public void saveTeacher(Teacher teacher) {
+        Teacher storedTeacher = findById(teacher.getId());
+
+        if (teacher.getPassword() == null || teacher.getPassword().isEmpty()) {
+            teacher.setPassword(storedTeacher.getPassword());
+        } else {
+            teacher.setPassword(encoder.encode(teacher.getPassword()));
+        }
+        teacherRepository.save(teacher);
+    }
+    public Iterable<Student> getStudents(long teacherId){
+        return findById(teacherId).getStudents();
     }
 }
