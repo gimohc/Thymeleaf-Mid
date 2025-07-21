ed8d209f605251d2349e30b5cdc099c333dbb968
gimohc <mrlenovog50@gmail.com>
2025-07-21 10:03:17 +0300

    Added custom query to get password from ID

--- src/main/java/com/training/thymeleafmid/student/StudentRepository.java
+++ src/main/java/com/training/thymeleafmid/student/StudentRepository.java
@@ -1,6 +1,9 @@
 package com.training.thymeleafmid.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s.password FROM Student s WHERE s.id = :id")
+    String findPasswordById(long id);
 }
