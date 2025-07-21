db085c87564e68b7b4079cbfe156110571ee7969
gimohc <mrlenovog50@gmail.com>
2025-07-21 12:11:13 +0300

    Updated StudentController

--- src/main/java/com/training/thymeleafmid/student/StudentController.java
+++ src/main/java/com/training/thymeleafmid/student/StudentController.java
@@ -56,7 +56,6 @@ public class StudentController {
 
         return "student/view";
     }
-    //TODO FIX logging in not working after certain amount of times... login issue
     @PostMapping("register")
     public String saveNewStudent(@ModelAttribute Student student){
         studentService.saveNewStudent(student);
