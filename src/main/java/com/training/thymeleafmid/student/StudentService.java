df4bf48e9b2d0f0f027e15e9ecf6876ded63c47b
gimohc <mrlenovog50@gmail.com>
2025-07-22 11:40:20 +0300

    Implemented spring security authentication

--- src/main/java/com/training/thymeleafmid/student/StudentService.java
+++ src/main/java/com/training/thymeleafmid/student/StudentService.java
@@ -1,16 +1,20 @@
 package com.training.thymeleafmid.student;
 
 import com.training.thymeleafmid.Exceptions.StudentNotFoundException;
-import com.training.thymeleafmid.LoginRequest;
 import com.training.thymeleafmid.teacher.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
-public class StudentService {
+public class StudentService implements UserDetailsService {
     private final StudentRepository studentRepository;
     private final PasswordEncoder encoder;
 
@@ -18,11 +22,6 @@ public class StudentService {
     public StudentService(StudentRepository studentRepository, PasswordEncoder passwordEncoder) {
         this.studentRepository = studentRepository;
         this.encoder = passwordEncoder;
-    }
-    public boolean validateLogin(LoginRequest request) {
-        String password = studentRepository.findPasswordById(request.getId());
-        return encoder.matches(request.getPassword(), password);
-
     }
     public Student findById(long id){
         Optional<Student> studentOpt = studentRepository.findById(id);
@@ -51,5 +50,22 @@ public class StudentService {
     public void saveNewStudent(Student student){
         student.setPassword(encoder.encode(student.getPassword()));
         studentRepository.save(student);
+    }
+
+//    public boolean validateLogin(LoginRequest request) {
+//        String password = studentRepository.findPasswordById(request.getId());
+//        return encoder.matches(request.getPassword(), password);
+//
+//    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Student student = studentRepository.findById(Long.valueOf(username))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with ID: " + username));
+
+        return new User(
+                String.valueOf(student.getId()),
+                student.getPassword(),
+                new ArrayList<>() // For this basic setup, we provide an empty list of roles (authorities).
+        );
     }
 }
