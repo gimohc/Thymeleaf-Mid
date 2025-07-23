aad8535dc6df8f85e4b6a394faf815635fce3bd3
gimohc <mrlenovog50@gmail.com>
2025-07-23 14:15:22 +0300

    Updated role entity to include set of teachers to remove them if that role was deleted.

--- src/main/java/com/training/thymeleafmid/admin/Role.java
+++ src/main/java/com/training/thymeleafmid/admin/Role.java
@@ -1,7 +1,13 @@
 package com.training.thymeleafmid.admin;
 
+import com.training.thymeleafmid.teacher.Teacher;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -9,8 +15,23 @@ import lombok.Data;
 public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
+    @Column(name = "role_id")
     private long id;
 
     @Column(unique = true, nullable = false)
     private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<Teacher> teachers = new HashSet<>();
+
+    @PreRemove
+    private void remoteTeachersFromRole() {
+        for(Teacher teacher: teachers) {
+            teacher.removeRole(this);
+        }
+    }
+
+
 }
