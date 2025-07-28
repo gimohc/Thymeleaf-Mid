a76cac671b7879f4f67fb03c6aedc4d76bb0cb0e
gimohc <mrlenovog50@gmail.com>
2025-07-28 14:51:20 +0300

    Added teacher constructor to TeacherDTO

--- src/main/java/com/training/thymeleafmid/teacher/TeacherDTO.java
+++ src/main/java/com/training/thymeleafmid/teacher/TeacherDTO.java
@@ -1,12 +1,22 @@
 package com.training.thymeleafmid.teacher;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class TeacherDTO {
     private String name;
     private String phoneNumber;
     private String password;
     private int workTime;
     private double hourlyPay;
+
+    public TeacherDTO(Teacher teacher) {
+        this.name = teacher.getUser().getName();
+        this.phoneNumber = teacher.getPhoneNumber();
+        this.password = teacher.getUser().getPassword();
+        this.workTime = teacher.getWorkTime();
+        this.hourlyPay = teacher.getHourlyPay();
+    }
 }
