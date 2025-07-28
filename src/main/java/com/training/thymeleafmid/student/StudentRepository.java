38a6d8f9a48573923bc2721fa45cdc60c13fe1cc
gimohc <mrlenovog50@gmail.com>
2025-07-28 09:52:02 +0300

    Removed extra query was not used

--- src/main/java/com/training/thymeleafmid/student/StudentRepository.java
+++ src/main/java/com/training/thymeleafmid/student/StudentRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    @Query("SELECT s.password FROM Student s WHERE s.id = :id")
-    String findPasswordById(long id);
 }
--- src/main/java/com/training/thymeleafmid/teacher/TeacherRepository.java
+++ src/main/java/com/training/thymeleafmid/teacher/TeacherRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-    @Query("SELECT t.password FROM Teacher t WHERE t.id = :id")
-    String findPasswordById(long id);
 }
