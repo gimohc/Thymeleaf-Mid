ab340ef3908d7d6cc8a3626a9ac5d05146346169
gimohc <mrlenovog50@gmail.com>
2025-07-28 14:28:59 +0300

    Changed authentication to return auth object for user information

--- src/main/java/com/training/thymeleafmid/config/AuthService.java
+++ src/main/java/com/training/thymeleafmid/config/AuthService.java
@@ -14,23 +14,16 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
-
     @Autowired
     public AuthService(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
-
     }
 
-    public void setCookie (HttpServletResponse response, LoginRequest request, boolean type) {
+    public Authentication authenticateAndSetCookie(HttpServletResponse response, LoginRequest request) {
 
-    }
-
-    // true for student, false for teacher
-    public void authenticateAndSetCookie(HttpServletResponse response, LoginRequest request, boolean type) {
-        String userType = type ? "student:" : "teacher:";
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(userType+request.getId(), request.getPassword())
+                new UsernamePasswordAuthenticationToken(request.getId(), request.getPassword())
         );
         String jwt = jwtUtil.generateToken((UserDetails) authentication.getPrincipal());
         jakarta.servlet.http.Cookie cookie = new jakarta.servlet.http.Cookie("token", jwt);
@@ -38,6 +31,7 @@ public class AuthService {
         cookie.setPath("/");
         cookie.setMaxAge(60 * 60);
         response.addCookie(cookie);
+        return authentication;
     }
     public void deleteCookie(HttpServletResponse response) {
         jakarta.servlet.http.Cookie cookie = new jakarta.servlet.http.Cookie("token", null);
