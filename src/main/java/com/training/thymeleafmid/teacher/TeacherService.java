da6e5e326a611ead5ca379b687855b2761622ebf
gimohc <mrlenovog50@gmail.com>
2025-07-28 14:31:38 +0300

    Changed logic to use entity DTO for creation and update

--- src/main/java/com/training/thymeleafmid/teacher/TeacherService.java
+++ src/main/java/com/training/thymeleafmid/teacher/TeacherService.java
@@ -1,10 +1,13 @@
 package com.training.thymeleafmid.teacher;
 
 import com.training.thymeleafmid.Exceptions.RoleNotFoundException;
+import com.training.thymeleafmid.Exceptions.UserNotFoundException;
 import com.training.thymeleafmid.admin.Role;
 import com.training.thymeleafmid.admin.RoleRepository;
 import com.training.thymeleafmid.student.Student;
 import com.training.thymeleafmid.student.StudentService;
+import com.training.thymeleafmid.user.User;
+import com.training.thymeleafmid.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,14 +23,16 @@ public class TeacherService {
     private final PasswordEncoder encoder;
     private final StudentService studentService;
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public TeacherService(TeacherRepository teacherRepository, StudentService studentService
-                          , PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
+                          , PasswordEncoder passwordEncoder, RoleRepository roleRepository, UserRepository userRepository) {
         this.teacherRepository = teacherRepository;
         this.encoder = passwordEncoder;
         this.studentService = studentService;
         this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
     }
     public Teacher findById(long id){
         Optional<Teacher> teacherOpt = teacherRepository.findById(id);
@@ -54,27 +59,46 @@ public class TeacherService {
     public Iterable<Teacher> findAll() {
         return teacherRepository.findAll();
     }
-    public Teacher saveNewTeacher(Teacher teacher) {
-        teacher.setPassword(encoder.encode(teacher.getPassword()));
-        Role role = roleRepository.findByName("ROLE_TEACHER").orElseThrow(RoleNotFoundException::new);
-        teacher.addRole(role);
-        teacherRepository.save(teacher);
-        return teacher;
-    }
-    public void saveTeacher(Teacher teacher) {
-        Teacher storedTeacher = findById(teacher.getId());
+    @Transactional
+    public Teacher saveNewTeacher(TeacherDTO request) {
+        User user = new User();
+        Teacher teacher = new Teacher();
+        Role role = roleRepository.findByName("ROLE_TEACHER")
+                .orElseThrow(() -> new RoleNotFoundException("Role not found"));
 
-        if (teacher.getPassword() == null || teacher.getPassword().isEmpty()) {
-            teacher.setPassword(storedTeacher.getPassword());
-        } else {
-            teacher.setPassword(encoder.encode(teacher.getPassword()));
+        user.addRole(role);
+        user.setPassword(encoder.encode(request.getPassword()));
+        user.setName(request.getName());
+
+
+        teacher.setPhoneNumber(request.getPhoneNumber());
+        teacher.setHourlyPay(request.getHourlyPay());
+        teacher.setWorkTime(request.getWorkTime());
+        teacher.setUser(user);
+        user.setTeacherProfile(teacher);
+
+        return teacherRepository.save(teacher);
+    }
+    @Transactional
+    public void updateTeacher(long userId, TeacherDTO request) {
+        User userToUpdate = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        userToUpdate.setName(request.getName());
+
+        if (request.getPassword() != null && !request.getPassword().isEmpty()) {
+            userToUpdate.setPassword(encoder.encode(request.getPassword()));
         }
-        teacherRepository.save(teacher);
+        Teacher profile = userToUpdate.getTeacherProfile();
+        profile.setPhoneNumber(request.getPhoneNumber());
+        profile.setHourlyPay(request.getHourlyPay());
+        profile.setWorkTime(request.getWorkTime());
+
     }
     public Teacher authenticate(Authentication authentication) {
         if(authentication == null) return null;
         UserDetails details = (UserDetails) authentication.getPrincipal();
-        long teacherId = Long.parseLong(details.getUsername().split(":")[1]);
+        long teacherId = Long.parseLong(details.getUsername());
         return findById(teacherId);
     }
 }
