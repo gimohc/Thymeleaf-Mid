3c8719514ffce83f0e6214538914050c6f3e30ff
gimohc <mrlenovog50@gmail.com>
2025-07-28 14:47:26 +0300

    Added student constructor to StudentDTO class

--- src/main/java/com/training/thymeleafmid/student/StudentDTO.java
+++ src/main/java/com/training/thymeleafmid/student/StudentDTO.java
@@ -2,11 +2,20 @@ package com.training.thymeleafmid.student;
 
 import com.training.thymeleafmid.teacher.Teacher;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class StudentDTO {
     private String name;
     private String phoneNumber;
     private String password;
     private Teacher teacher;
+
+    public StudentDTO(Student student) {
+        this.name = student.getUser().getName();
+        this.phoneNumber = student.getPhoneNumber();
+        this.password = student.getUser().getPassword();
+        this.teacher = student.getTeacher();
+    }
 }
