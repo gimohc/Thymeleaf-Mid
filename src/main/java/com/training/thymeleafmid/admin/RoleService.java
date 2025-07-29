fed027bac0083087e6078ed50f71204764b50d04
gimohc <mrlenovog50@gmail.com>
2025-07-29 10:06:15 +0300

    Refactored RoleService added comments and enhanced readability

--- src/main/java/com/training/thymeleafmid/admin/RoleService.java
+++ src/main/java/com/training/thymeleafmid/admin/RoleService.java
@@ -3,9 +3,7 @@ package com.training.thymeleafmid.admin;
 import com.training.thymeleafmid.Exceptions.RoleNotFoundException;
 import com.training.thymeleafmid.Exceptions.UserNotFoundException;
 import com.training.thymeleafmid.student.Student;
-import com.training.thymeleafmid.student.StudentService;
 import com.training.thymeleafmid.teacher.Teacher;
-import com.training.thymeleafmid.teacher.TeacherService;
 import com.training.thymeleafmid.user.User;
 import com.training.thymeleafmid.user.UserRepository;
 import jakarta.transaction.Transactional;
@@ -33,31 +31,32 @@ public class RoleService {
     public Iterable<Role> findAll() {
         return roleRepository.findAll();
     }
-    public Role findById(long id) {
-        return roleRepository.findById(id).orElse(null);
+    public Role findById(long id) throws RoleNotFoundException {
+        return roleRepository.findById(id)
+                .orElseThrow(() -> new RoleNotFoundException("Role not found"));
     }
 
-    public void removeRolesFromUser(long teacherId) {
-        User user = userRepository.findById(teacherId).orElseThrow(() -> new UserNotFoundException("User not found"));
-        user.clearRoles();
+    public void removeRolesFromUser(long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+        user.clearRoles(); // only need to clear the owning side
         userRepository.save(user);
     }
 
-    @Transactional
+    @Transactional // assign role to user
     public void giveRole(long roleId, long userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found"));
-        Role role = findById(roleId);
-        if(role == null) throw new RoleNotFoundException("Role not found");
-        if(!user.getRoles().contains(role)) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+        Role role = findById(roleId); // already throws an exception if the role does not exist
+
+        if(!user.getRoles().contains(role)) { // check if the user already has that role
             user.addRole(role);
             if(role.getName().equals("ROLE_STUDENT") && user.getStudentProfile() == null) {
-                Student student = new Student();
-                user.setStudentProfile(student);
-                student.setUser(user);
+                // if a student role was given, give the user a new student profile
+                user.setStudentProfile(new Student()); // bidirectional relation is configured in the setter
             } else if(role.getName().equals("ROLE_TEACHER") && user.getTeacherProfile() == null) {
-                Teacher teacher = new Teacher();
-                teacher.setUser(user);
-                user.setTeacherProfile(teacher);
+                // if a teacher role was given, give the user a new teacher profile
+                user.setTeacherProfile(new Teacher()); // bidirectional relation is configured in the setter
             }
 
         }
