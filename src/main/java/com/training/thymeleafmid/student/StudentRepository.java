ff1c4e86360ed103d897dc395e04938f8fb6bd80
gimohc <mrlenovog50@gmail.com>
2025-07-29 10:51:49 +0300

    Removed extra import

--- src/main/java/com/training/thymeleafmid/student/StudentRepository.java
+++ src/main/java/com/training/thymeleafmid/student/StudentRepository.java
@@ -1,7 +1,6 @@
 package com.training.thymeleafmid.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
