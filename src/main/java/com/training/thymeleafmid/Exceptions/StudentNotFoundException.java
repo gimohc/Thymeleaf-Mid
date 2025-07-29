53b9ec8417677eb957d3754ef0ae6cc3da5d12f7
gimohc <mrlenovog50@gmail.com>
2025-07-29 10:31:21 +0300

    Deleted extra unused exceptions

--- src/main/java/com/training/thymeleafmid/Exceptions/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.training.thymeleafmid.Exceptions;
-
-public class StudentNotFoundException extends RuntimeException{
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
